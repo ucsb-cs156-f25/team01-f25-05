18cfe5d7a536ddc4ac190e834fc187a41e23fef5
hechenjin <hechenjin@ucsb.edu>
2025-10-28 04:16:17 -0700

    HY-testvoverage for controller

--- src/main/java/edu/ucsb/cs156/example/controllers/RecommendationRequestController.java
+++ src/main/java/edu/ucsb/cs156/example/controllers/RecommendationRequestController.java
@@ -2,22 +2,17 @@ package edu.ucsb.cs156.example.controllers;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
 import edu.ucsb.cs156.example.entities.RecommendationRequest;
-import edu.ucsb.cs156.example.errors.EntityNotFoundException;
 import edu.ucsb.cs156.example.repositories.RecommendationRequestRepository;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.Parameter;
 import io.swagger.v3.oas.annotations.tags.Tag;
-import jakarta.validation.Valid;
 import java.time.LocalDateTime;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.security.access.prepost.PreAuthorize;
-import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -42,24 +37,6 @@ public class RecommendationRequestController extends ApiController {
   public Iterable<RecommendationRequest> allUCSBDates() {
     Iterable<RecommendationRequest> reqs = recReqRepository.findAll();
     return reqs;
-  }
-
-  /**
-   * Get a single request by id
-   *
-   * @param id the id of the date
-   * @return a Recommendation Request
-   */
-  @Operation(summary = "Get a single Recommendation Request")
-  @PreAuthorize("hasRole('ROLE_USER')")
-  @GetMapping("")
-  public RecommendationRequest getById(@Parameter(name = "id") @RequestParam Long id) {
-    RecommendationRequest req =
-        recReqRepository
-            .findById(id)
-            .orElseThrow(() -> new EntityNotFoundException(RecommendationRequest.class, id));
-
-    return req;
   }
 
   /**
@@ -113,55 +90,5 @@ public class RecommendationRequestController extends ApiController {
     RecommendationRequest saved = recReqRepository.save(recReq);
 
     return saved;
-  }
-
-  /**
-   * Delete a Recommendation Request
-   *
-   * @param id the id of the Recommendation Request to delete
-   * @return a message indicating the Recommendation Request was deleted
-   */
-  @Operation(summary = "Delete a Recommendation Request")
-  @PreAuthorize("hasRole('ROLE_ADMIN')")
-  @DeleteMapping("")
-  public Object deleteRecommendationRequest(@Parameter(name = "id") @RequestParam Long id) {
-    RecommendationRequest req =
-        recReqRepository
-            .findById(id)
-            .orElseThrow(() -> new EntityNotFoundException(RecommendationRequest.class, id));
-
-    recReqRepository.delete(req);
-    return genericMessage("Recommendation Request with id %s deleted".formatted(id));
-  }
-
-  /**
-   * Update a single Recommendation Request
-   *
-   * @param id id of the Recommendation Request to update
-   * @param newReqest the new Request
-   * @return the updated date object
-   */
-  @Operation(summary = "Update a single Recommendation Request")
-  @PreAuthorize("hasRole('ROLE_ADMIN')")
-  @PutMapping("")
-  public RecommendationRequest updateRecommendationRequest(
-      @Parameter(name = "id") @RequestParam Long id,
-      @RequestBody @Valid RecommendationRequest newReqest) {
-
-    RecommendationRequest recReq =
-        recReqRepository
-            .findById(id)
-            .orElseThrow(() -> new EntityNotFoundException(RecommendationRequest.class, id));
-
-    recReq.setRequesterEmail(newReqest.getRequesterEmail());
-    recReq.setProfessorEmail(newReqest.getProfessorEmail());
-    recReq.setExplanation(newReqest.getExplanation());
-    recReq.setDateRequested(newReqest.getDateRequested());
-    recReq.setDateNeeded(newReqest.getDateNeeded());
-    recReq.setDone(newReqest.getDone());
-
-    recReqRepository.save(recReq);
-
-    return recReq;
   }
 }
